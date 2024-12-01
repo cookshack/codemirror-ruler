76d4a6a3e828f05fae16b83b5b459e4c8ceccf22
Matt Mundell <matt@mundell.me>
2024-12-01 17:19:31 +0200

    check on commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 export default {
   input: 'index.js',
-  external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
   output: [ { file: 'dist/index.cjs', format: 'cjs' },
             { dir: './dist', format: 'es' } ],
   plugins: []
