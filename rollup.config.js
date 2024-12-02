90450c3cf6051c9829d9a7bcb4ef348944fff94e
Matt Mundell <matt@mundell.me>
2024-12-02 14:01:53 +0200

    add back external

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 export default {
   input: 'index.js',
+  external: /^(?!\.?\/)/, // internal if starts with ./ or /
   output: [ { file: 'dist/index.cjs', format: 'cjs' },
             { dir: './dist', format: 'es' } ],
   plugins: []
